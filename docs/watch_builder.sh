68a9e096e53b6ce83fc43982b9827cc74186f091
Maximiliano <maximiliano.romay@autodesk.com>
2020-10-24 00:39:20 -0300

    * new script for starting, stopping and restarting the docs build

--- docs/watch_builder.sh
+++ docs/watch_builder.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-ps aux | grep SimpleHTTPServer | grep -v grep | awk '{print $2}' | while read pid; do kill -9 $pid; done
-ps aux | grep "watch .. . make html" | grep -v grep | awk '{print $2}' | while read pid; do kill -9 $pid; done
+function stop() {
+    ps aux | grep SimpleHTTPServer | grep -v grep | awk '{print $2}' | while read pid; do kill -9 $pid; done
+    ps aux | grep "watch .. . make html" | grep -v grep | awk '{print $2}' | while read pid; do kill -9 $pid; done
+}
 
-cd build/html
+function start() {
+    cd build/html
+    /usr/bin/python2 -m SimpleHTTPServer > /dev/null 2>&1 &
+    cd ../../
+    watch -n 1 "make html" > /dev/null 2>&1 &
+}
 
-/usr/bin/python2 -m SimpleHTTPServer > /dev/null 2>&1 &
+function restart() {
+    stop
+    start
+}
 
-cd ..
-
-watch -n 2 "make html" > /dev/null 2>&1 &
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        restart
+        ;;
+    *)
+        echo "start|stop|restart"
+        ;;
+esac
