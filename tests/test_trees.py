c9e117aaefdd99c657433410853d80f6f532f874
Maximiliano <maximiliano.romay@autodesk.com>
2020-10-24 03:36:53 -0300

    * new test for the BinaryTree

--- tests/test_trees.py
+++ tests/test_trees.py
@@ -1,17 +1,40 @@
+"""
+Tests for the jobfinder.trees module
+"""
 from unittest import TestCase
 from jobfinder import trees
 from jobfinder.trees import BinaryTree
 
 
-class TestTree(TestCase):
+class TestBinaryTree(TestCase):
+    """
+    Tests for the BinaryTree class
+    """
 
     def test_new(self):
+        """
+        New must return instance of type BinaryTree
+        """
         tree = trees.new()
         self.assertIsInstance(tree, BinaryTree)
 
-    def test_insert(self):
+    def test_insert_new_object(self):
+        """
+        Insert must return a new object
+        """
         tree = trees.new()
         element = 1
         tree_2 = trees.insert(tree, element)
-        self.assertIsInstance(tree, BinaryTree)
         self.assertIsNot(tree, tree_2)
+
+    def test_insert_find_element(self):
+        """
+        After inserting element we can find it
+        """
+        tree = trees.new()
+        element = 1
+        tree_2 = trees.insert(tree, element)
+        found = trees.find(tree_2, element)
+        self.assertEqual(found, True)
+        found = trees.find(tree_2, 2)
+        self.assertEqual(found, False)
