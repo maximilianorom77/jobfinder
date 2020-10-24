0d6e40220440987d879ecf83686db175116e2779
Maximiliano <maximiliano.romay@autodesk.com>
2020-10-24 03:16:42 -0300

    * new tree interface

---
+++ jobfinder/trees.py
@@ -0,0 +1,22 @@
+"""
+The module to define trees
+"""
+
+
+class BinaryTree:
+    """
+    Standard binary tree
+    """
+    node = None
+
+
+def new() -> BinaryTree:
+    """
+    Standard binary tree.
+    """
+    tree = BinaryTree()
+    return tree
+
+def insert(tree: BinaryTree, element) -> BinaryTree:
+    tree = BinaryTree()
+    return tree
