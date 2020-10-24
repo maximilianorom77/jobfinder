5a004925b8497de38bdd9c24febc555e01f7c1c8
Maximiliano <maximiliano.romay@autodesk.com>
2020-10-24 02:06:36 -0300

    * new module scrapper.partition

---
+++ scrapper/partition.py
@@ -0,0 +1,9 @@
+"""
+The module contains the classes and functions to partition the jobs by using a
+binary tree and other structures.
+"""
+
+class BinaryTree:
+    """
+    Standard binary tree.
+    """
